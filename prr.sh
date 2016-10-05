b5b7c790d6661bfb48181bb729c253250150137e
Ryan Wholey <ryan@eventbrite.com>
2016-10-05 15:43:15 -0700

    update

--- prr.sh
+++
@@ -1,7 +0,0 @@
-cp .gitignore .original
-cat .gitignore | awk '!/remove_for_release/' > .ignoretemp
-cat .ignoretemp > .gitignore
-rm .ignoertemp
-echo .original >> .gitignore
-git rm --cached -q -r .
-git add dist
