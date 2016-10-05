4f6ddad8328f5c84379f41890d1238d9b266a946
Ryan Wholey <ryan@eventbrite.com>
2016-10-05 14:05:12 -0700

    add task

--- src/tasks/release.js
+++ src/tasks/release.js
@@ -18,16 +18,57 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.config.set('gitadd', {
+        task: {
+            options: {
+                message: 'adding dist..',
+                force: true
+            }
+        },
+        files: {
+            src: ['./dist']
+        }
+    });
+
+    grunt.config.set('gitcommit', {
+        task: {
+            options: {
+                message: 'committing dist..',
+                force: true
+            }
+        },
+        files: {
+            src: ['./dist']
+        }
+    });
+
     grunt.registerTask('publish', 'Publish package to reggie', function() {
 
+        // var pkg = grunt.file.readJSON('package.json'),
+        //     cmd = './node_modules/reggie/client.js -u ' + pkg.publishConfig.registry + ' publish';
+
+        // if (grunt.option('no-write')) {
+        //     grunt.log.ok('---DRY-RUN----');
+        //     grunt.log.ok(cmd);
+        // } else {
+        //     grunt.log.ok(cmd);
+        //     shell.exec(cmd);
+        // }
+        // grunt.log.ok('Published to Reggie');
     });
 
     grunt.registerTask('release', function() {
+
+        shell.exec('')
 
         var bumpTask = 'bump-version' + Array.prototype.slice.call(arguments).map(function(val) {
             return ':' + val;
         });
 
         grunt.task.run(bumpTask, 'publish');
+    });
+
+    grunt.reigsterTask('add-dist', function() {
+        grunt.task.run('grunt-git')
     });
 };
