daff20fdafd9e66a6692f0e2d91e82715815e10c
Ryan Wholey <ryan@eventbrite.com>
2016-10-05 14:39:25 -0700

    changes

--- push_dist.sh
+++ push_dist.sh
@@ -3,8 +3,8 @@ cp .gitignore gitignore
 cat .gitignore | awk '!/**remove_for_build**/' > .gitignore
 echo gitignore >> .gitignore
 
-git add ./dist
-git commit -m "adding dist for release"
+git add -f ./dist
+git commit -f -m "adding dist for release"
 git push -f origin master
 
 cat gitignore > .gitignore
