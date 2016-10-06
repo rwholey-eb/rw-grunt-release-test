88a731fa56fdab5488aa594e11c0834aeadd300f
Ryan Wholey <ryan@eventbrite.com>
2016-10-05 22:19:40 -0700

    update

--- prr.sh
+++ prr.sh
@@ -1,16 +1,3 @@
-cp .gitignore .original
-cat .gitignore | awk '!/dist/' > .ignoretemp
-cat .ignoretemp > .gitignore
-rm .ignoretemp
-echo .original >> .gitignore
-git rm --cached -q -r ./dist
-
-git add .
+git add -f dist
 git commit -m "pushing dist"
 git push origin master
-
-git status
-
-cat .original > .gitignore
-rm .original
-git add .
